60ab920f72a5bb293cc3ae0c2098aa7e865df47a
RichoM <richi.moran@gmail.com>
2017-10-12 09:30:51 -0300

    First incomplete version.

---
+++ 5EnRaya/Assets/Scripts/Model/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game
+{
+    private Board board;
+    private Player[] players;
+    private int turn = 0;
+    public Game(Board board, Player[] players)
+    {
+        this.board = board;
+        this.players = players;
+    }
+
+    public Board Board { get { return board; } }
+    public IEnumerable<Player> Players { get { return players; } }
+    public int Turn { get { return turn; } }
+    public Player CurrentPlayer { get { return players[turn]; } }
+    
+    public int IndexOfPlayer(Player player)
+    {
+        return Array.IndexOf(players, player);
+    }
+
+    public void Play(int column)
+    {
+        try
+        {
+            CurrentPlayer.Play(column, board);
+            NextTurn();
+        }
+        catch (InvalidOperationException)
+        {
+            // Do nothing
+        }
+    }
+
+    private void NextTurn()
+    {
+        turn = (turn + 1) % players.Length;
+    }
+}
