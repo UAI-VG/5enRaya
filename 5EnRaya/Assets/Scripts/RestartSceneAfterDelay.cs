e44ee61bf029dac39d6ebbeec983ccea28ad900d
RichoM <richi.moran@gmail.com>
2017-10-12 22:24:10 -0300

    Added a small script that would restart the scene after a delay.
    I will use this on the game scene to show a brief message when the game is
    over and then simply restart the scene.

---
+++ 5EnRaya/Assets/Scripts/RestartSceneAfterDelay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartSceneAfterDelay : MonoBehaviour
+{
+    public float seconds;
+
+    void Start()
+    {
+        StartCoroutine(RestartScene());
+    }
+
+    IEnumerator RestartScene()
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
